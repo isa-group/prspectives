31043a273bf9a5a8596f2f50e9d5cb44427b98a3
Francisco Martinez Espartero <francisco.mar.esp#gmail.com>
2014-03-15 12:27:49 +0100

    partial implementation of upload files with angular.js

--- prspectives/src/main/webapp/templates/templates.js
+++ prspectives/src/main/webapp/templates/templates.js
@@ -1,4 +1,4 @@
-angular.module('templatesApp', ['ppinotTemplates', 'loginModule', 'navbarModule']);
+angular.module('templatesApp', ['ppinotTemplates', 'loginModule', 'navbarModule', 'angularFileUpload']);
 
 function iteratePPIs(ppiSet, iterationFunction) {
     $(ppiSet).each(function() {
@@ -92,3 +92,33 @@ function TemplatesCtrl($scope, $location, $http) {
         $http.put($scope.model.url+"/ppis", $scope.ppis);
     }
 }
+
+var MyCtrl = [ '$scope', '$upload', function($scope, $upload) {
+  $scope.onFileSelect = function($files) {
+    //$files: an array of files selected, each file has name, size, and type.
+    for (var i = 0; i < $files.length; i++) {
+      var file = $files[i];
+      $scope.upload = $upload.upload({
+        url: 'server/upload/url', //upload.php script, node.js route, or servlet url
+        // method: POST or PUT,
+        // headers: {'headerKey': 'headerValue'},
+        // withCredentials: true,
+        data: {myObj: $scope.myModelObj},
+        file: file,
+        // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
+        /* set file formData name for 'Content-Desposition' header. Default: 'file' */
+        //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
+        /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
+        //formDataAppender: function(formData, key, val){} //#40#issuecomment-28612000
+      }).progress(function(evt) {
+        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+      }).success(function(data, status, headers, config) {
+        // file is uploaded successfully
+        console.log(data);
+      });
+      //.error(...)
+      //.then(success, error, progress); 
+    }
+    // $scope.upload = $upload.upload({...}) alternative way of uploading, sends the the file content directly with the same content-type of the file. Could be used to upload files to CouchDB, imgur, etc... for HTML5 FileReader browsers. 
+  };
+}];
